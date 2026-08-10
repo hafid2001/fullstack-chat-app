1d189166c85b9f06d5f406fcfdf70f8313ea18de
matela <abdou26072001@gmail.com>
2026-08-10 17:56:50 +0100

    correct the error in profile comp

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,32 +1,42 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+import assets from "../assets/assets";
+
 const ProfilePage = () => {
-const [selectedUser, setSelectedUser] = useState(false);
-const navigate = useNavigate();
-const [name,setName] = useState("Ahmd Ali");
-const [bio,setBio] = useState("I am a software engineer");
-return(
-  <div className='min-h-secreen bg-cover bg-no-repeat flex items-center justify-center'>
-    <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg' >
-    <form className="flex flex-col gap-5 p-10 flex-1">
-      <h3 className="text-lg">Profile details</h3>
-      <label htmlFor="avatar" className='flex items-center gap3 cursor-pointer'>
-        <input type="file" id="avatar" accept=".png, .jpg,.jpeg "hidden/>
-        <img src={""} alt=""/>
-      </label>
+  const [selectedImg, setSelectedImg] = useState();
+  //const navigate = useNavigate();
+  const [name, setName] = useState("Ahmd Ali");
+  const [bio, setBio] = useState("I am a software engineer");
 
-    </form>
-    <img src="" alt=""/>
+  return (
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon}
+              alt=""
+              className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}
+            />
+            upload profile image
+          </label>
+          
+
+
+
+        </form>
+      </div>
     </div>
-
-  </div>
-
-
-
-
-
-)
+  );
 };
 
 export default ProfilePage;
