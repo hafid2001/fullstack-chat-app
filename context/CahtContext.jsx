0c01ac891de7a6976171074d16d84738da696825
matela <abdou26072001@gmail.com>
2026-08-13 15:50:49 +0100

    function to get all users for sidebar

---
+++ context/CahtContext.jsx
@@ -0,0 +1,54 @@
+import { Children, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({children})=>{
+
+
+    const [messages , setMessages] = useState([]);
+    const [users,setUsers]= useState([]);
+    const [selectedUser,setSelectedUser]=useState(null);
+    const [unseenMessages,setUnseenMessages]=useState({});
+
+    const {socket , axios} = useContext(AuthContext);
+     
+    // function to get all users for sidebar 
+    const getUsers = async () =>{
+      try{
+        const {data} = await axios.get("api/messages/users");
+        if(data.success){
+            setUsers(data.users)
+            setUnseenMessages(data.unseenMessages)
+        }
+      }catch (error){
+        toast.error.(error.messages)
+      }
+    }
+    
+
+    const value={
+
+
+
+    }
+
+    return(
+   <ChatContext.ChatProvider value={value}>
+        {children}
+   </ChatContext.ChatProvider>
+
+
+
+    )
+
+
+
+
+
+
+
+
+}
